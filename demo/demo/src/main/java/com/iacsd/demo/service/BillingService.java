a04ae3f61509819842335c344cd28f22896bea83
aditya1305 <aditya.sahu199@gmail.com>
2022-09-28 18:05:01 +0530

    First Commit

---
+++ demo/demo/src/main/java/com/iacsd/demo/service/BillingService.java
@@ -0,0 +1,24 @@
+package com.iacsd.demo.service;
+
+import com.iacsd.demo.dto.bill.BillDTO;
+import com.iacsd.demo.dto.bill.BillListingDto;
+import com.iacsd.demo.dto.bill.ViewBillDto;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface BillingService {
+	String getBill(BillDTO request);
+
+    List<BillListingDto> getBills(String mobileNumber);
+
+    ViewBillDto getBillDtls(String uid);
+
+    //String getBillFreeMarker(BillDTO request);
+   // String getDownloadBills(String uid);
+
+    ResponseEntity<Resource> downloadBill(String uid);
+
+
+}
